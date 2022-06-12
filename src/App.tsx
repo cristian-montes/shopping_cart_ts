d3cc946ff32700216b21d4e304105284666ca96a
Cristian Montes <cmontesmujica@gmail.com>
2022-06-11 19:26:12 -0600

    added needed imports to app.tsx and also added the function to fetch data from the external API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
 
+// Components
+import { Drawer, LinearProgress, Grid, Badge } from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
+
+//Styles
+import { Wrapper } from "./App.styles";
+
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+
+const getProducts = async (): Promise<CartItemType[]> => 
+  await(await fetch('https://fakestoreapi.com/products')).json()  //first awai for the json() and the secong one for the fetch.
 
 const  App = () => {
+  const {data, isLoading, error } = useQuery<CartItemType[]>('products',getProducts );
+
+  console.log('adata', data);
+  
   return (
     <div className="App">
       Start
